1e11db59f7ef82785314f026c1a8888d8a0d622e
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-02-16 21:44:05 -0300

    feat: add routes, server.js, and edit package.json and .env

---
+++ src/routes/menuRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const menuController = require('../controllers/menuController.js');
+const listController = require('../controllers/listController.js');
+
+router.get('/menu', menuController.getMenu);
+router.get('/order', listController.getAllOrders);
+router.post('/order', listController.addOrder);
+router.delete('/order:id', listController.deleteOrder);
+router.get('/order:id', listController.getOrderById);
+
+module.exports = router;
