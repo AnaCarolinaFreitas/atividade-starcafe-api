a5d609142e668ca70698055b61bf28e7d7c47c8d
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-02-18 08:19:13 -0300

    fix: id at routes

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -6,7 +6,7 @@ const listController = require('../controllers/listController.js');
 router.get('/menu', menuController.getMenu);
 router.get('/order', listController.getAllOrders);
 router.post('/order', listController.addOrder);
-router.delete('/order:id', listController.deleteOrder);
-router.get('/order:id', listController.getOrderById);
+router.delete('/order/:id', listController.deleteOrder);
+router.get('/order/:id', listController.getOrderById);
 
 module.exports = router;
